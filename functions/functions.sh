2ac8b7cb08f8afac0fbe6bec2e97c560cf870504
Guilherme França <souzagfranca@gmail.com>
2024-08-20 13:00:10 -0300

    Modulação do SempreDeploy, e ajuste na função altera_inc_empresa

--- functions/functions.sh
+++ functions/functions.sh
@@ -61,10 +61,17 @@ altera_inc_empresa() {
     ssh sempre@$HOST_ADDRESS <<EOF
     awk '
 /switch\(/ {in_switch=1}
-in_switch && /{/ {print; print "            case '\''$LINK'\'': return $DLPD_NO_ZEROS;"print ""; in_switch=0; next}
+in_switch && /{/ {
+	print
+	print "            case '\''TESTE'\'': return 9800;"
+	print ""
+	in_switch=0
+	next
+}
 {print}
 ' /var/www/html/sempre/includes/ws/inc_empresa.php > /var/www/html/sempre/includes/ws/inc_empresa_temp.php
 mv /var/www/html/sempre/includes/ws/inc_empresa_temp.php /var/www/html/sempre/includes/ws/inc_empresa.php
+chmod -Rf 777 /var/www/html/sempre/includes/ws/inc_empresa.php
 EOF
     if [ $? -ne 0 ]; then
         return 1
